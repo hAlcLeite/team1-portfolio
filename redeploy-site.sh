37df9c575034f41d5efdbd1b49bed346ff1c2500
Henrique Leite <hleite@uwo.ca>
2026-07-12 18:50:34 -0400

    Updated redeploy.sh for ststemct

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
-# 1. Kill all existing tmux sessions
-tmux kill-server 2>/dev/null
-
-# 2. cd into your project folder
+# 1. cd into your project folder
 cd ~/team1-portfolio-henrique || exit
 
-# 3. Grab the latest changes from GitHub and force reset
+# 2. Grab the latest changes from GitHub and force reset
 git fetch && git reset origin/main --hard
 
-# 4. Create virtual environment if it doesn't exist, then activate and install deps
-if [ ! -d "python3-virtualenv" ]; then
-  python3 -m venv python3-virtualenv
-fi
+# 3. Ensure dependencies are up to date
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
 
-# 5. Start a new detached Tmux session, activate the venv, and start the Flask server
-tmux new-session -d -s flask_app 'source python3-virtualenv/bin/activate && flask run --host=0.0.0.0'
+# 4. Restart the systemd service to pick up changes
+systemctl restart myportfolio
